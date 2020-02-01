76dd4e966df62ee3953168e9a2e0c99d17e43b82
Allan Barroso <allan.barroso@outlook.com>
2020-02-01 18:35:52 -0300

    Pattern design
    creating the pattern to start the code

---
+++ ui_tests/features/testfeature01.feature
@@ -0,0 +1,7 @@
+Feature: Just a test
+   test if it is working.
+
+Scenario: just test the navigation
+        Given navigate to the site http://automationpractice.com/index.php
+        When verify the page title
+        Then page sould be My Store
---
+++ ui_tests/steps/testfeature01.py
@@ -0,0 +1,13 @@
+from behave import *
+
+@given('navigate to the site http://automationpractice.com/index.php')
+def step_impl(context):
+    pass
+
+@when('verify the page title')
+def step_impl(context):
+    pass
+
+@then('page sould be "My Store"')
+def step_impl(context):
+    pass
