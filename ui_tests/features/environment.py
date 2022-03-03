4f3d2d504636fed381d9c2677db3b998e27fecad
Allan Barroso <abarroso@redhat.com>
2022-03-03 16:34:46 -0300

    test01

--- ui_tests/features/environment.py
+++ ui_tests/features/environment.py
@@ -1,18 +1,31 @@
+import platform
+from unittest import case
 from selenium import webdriver
- 
+
+
+
+os_info = platform.system()
+driver_path="../driver/"
+
+
 def before_scenario(context, scenario):
+  
+  if (os_info == "Linux"):
+        pass  
+          
+     
+
   if 'web' in context.tags:
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     context.browser = webdriver.Chrome(chrome_options=chrome_options,
-                              executable_path='/home/abarroso/CIandTgithub/CIandT/ui_tests/drivers/chromedriver')
-    #context.browser = webdriver.Crome(executable_path='/home/abarroso/CIandTgithub/CIandT/uitests-old/drivers/chromedriver')
+                              executable_path='../drivers/chromedriver')
+
     context.browser.implicitly_wait(10)
  
 def after_scenario(context, scenario):
   if 'web' in context.tags:
     context.browser.quit()
-
 
 
